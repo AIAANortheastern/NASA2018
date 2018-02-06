9e4c2e1aef077fa1db0ba957cd34d5a47479eadd
tykosa <kosakowski.t@husky.neu.edu>
2018-02-06 18:36:22 -0500

    subscale folder and accelerometer
    additional changes

--- Accelerometer_test/Accelerometer_test.ino
+++ Accelerometer_test/Accelerometer_test.ino
@@ -1,17 +1,24 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include "Wire.h"
+#include "SoftwareSerial.h"
 MPU6050 accelgyro;
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 #define OUTPUT_READABLE_ACCELGYRO
 #define LED_PIN 13
 bool blinkState = false;
+
+const int RXPin = A2;
+const int TXPin = A3;
+
+  SoftwareSerial Xbee(RXPin, TXPin);
+
  
 void setup() {
-  
+
         Wire.begin();
-    Serial.begin(9600);
+    Xbee.begin(9600);
     accelgyro.initialize();
     pinMode(LED_PIN, OUTPUT);
 }
