b88217dfc60629b592416886286b3a17cdc0dddb
tykosa <kosakowski.t@husky.neu.edu>
2018-02-06 19:45:25 -0500

    Acceleration Function
    moved the accerometer code to a function and streamlined it

--- Accelerometer_test/Accelerometer_test.ino
+++ Accelerometer_test/Accelerometer_test.ino
@@ -4,36 +4,32 @@
 #include "SoftwareSerial.h"
 MPU6050 accelgyro;
 int16_t ax, ay, az;
-int16_t gx, gy, gz;
 #define OUTPUT_READABLE_ACCELGYRO
-#define LED_PIN 13
-bool blinkState = false;
 
 const int RXPin = A2;
 const int TXPin = A3;
 
-  SoftwareSerial Xbee(RXPin, TXPin);
+SoftwareSerial Xbee(RXPin, TXPin);
 
  
 void setup() {
 
-        Wire.begin();
-    Xbee.begin(9600);
+    Wire.begin();
+    Serial.begin(9600);
     accelgyro.initialize();
-    pinMode(LED_PIN, OUTPUT);
 }
  
 void loop() {
  
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-          int start=millis();Serial.print(start);Serial.print("\t");
+        accel_X_Y_Z();
+
+        delay(100);
+}
+
+void accel_X_Y_Z(){
+        accelgyro.getAcceleration(&ax, &ay, &az);
         Serial.print(ax); Serial.print("\t");
         Serial.print(ay); Serial.print("\t");
-        Serial.print(az); Serial.print("\t");
-        Serial.print(gx); Serial.print("\t");
-        Serial.print(gy); Serial.print("\t");
-        Serial.println(gz);
- 
-    blinkState = !blinkState;
-    digitalWrite(LED_PIN, blinkState);
+        Serial.print(az); Serial.println("\t");
 }
+
