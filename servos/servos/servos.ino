3d9ffc5102a95e2f9d16f3e7ed08e7c29be90c54
tykosa <kosakowski.t@husky.neu.edu>
2018-02-20 19:13:13 -0500

    added servo code

---
+++ servos/servos/servos.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>
+
+Servo myservo;
+
+const int ServoPin = 1;
+
+void setup() {
+    myservo.attach(ServoPin);
+}
+
+void loop() {
+
+    myservo.attach(ServoPin);
+
+    delay(15);
+
+    myservo.write(1);
+
+    myservo.detach();
+
+    delay(1000);
+    
+    myservo.attach(ServoPin);
+
+    delay(15);
+
+    myservo.write(179);
+
+    delay(1000);
+
+    myservo.detach();
+
+    delay(1000);
+    
+}
